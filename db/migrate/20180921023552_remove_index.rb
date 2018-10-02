8ce3eda45fea281149a7c29f528c1e46f8b367c8
Priyankha <pri12dec@gmail.com>
2018-10-02 02:36:34 -0400

    remove index

--- db/migrate/20180921023552_remove_index.rb
+++ db/migrate/20180921023552_remove_index.rb
@@ -2,7 +2,7 @@ class RemoveIndex < ActiveRecord::Migration[5.2]
   def change
     remove_index :houses , column: :real_estate_company_id
     remove_index :houses , column: :realtor_id
-    remove_index :houses , column:  :realtors_id
+    #remove_index :houses , column:  :realtors_id
     remove_index :inquiries , column: :house_id
     remove_index :inquiries , column: :hunter_id
     remove_index :realtors , column: :real_estate_company_id
